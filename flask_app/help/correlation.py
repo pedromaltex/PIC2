d2e04498f370648fe9559aabeaf3b4d8dd9926fc
Pedro Maltez - Ubuntu <pedromaltez16.1@gmail.com>
2025-04-05 22:04:17 +0100

    new images to discuss

--- flask_app/help/correlation.py
+++ flask_app/help/correlation.py
@@ -3,13 +3,13 @@ import pandas as pd
 from datetime import datetime, timezone
 
 # Empresa alvo
-target_ticker = "AAPL"
+target_ticker = "^GSPC"
 # Índice de mercado
-market_ticker = "TSLA"
+market_ticker = "GLD"
 
 day, month, year = datetime.now().day, datetime.now().month, datetime.now().year
-start_date_1 = f"{year-1}-{month}-{day}"
-end_date = f"{year}-{month}-{day}"
+start_date_1 = f"{year-15}-{month}-{day}"
+end_date = f"{year-10}-{month}-{day}"
 
 # Baixar os dados históricos para os dois períodos
 data1 = yf.download(target_ticker, start=start_date_1, end=end_date)["Close"]
@@ -25,3 +25,24 @@ returns = data.pct_change().dropna()
 correlation = returns.corr().iloc[0, 1]
 
 print(f"Correlação de {target_ticker} com {market_ticker}: {correlation:.4f}")
+'''
+import yfinance as yf
+import pandas as pd
+from datetime import datetime
+
+# Ativos para comparar (exemplo: Apple e Ouro)
+ticker1 = "AAPL"
+ticker2 = "GLD"  # SPDR Gold Trust (ETF de ouro)
+
+# Baixar os dados históricos
+day, month, year = datetime.now().day, datetime.now().month, datetime.now().year
+data = yf.download([ticker1, ticker2], start=f"{year-20}-{month}-{day}", end=f"{year}-{month}-{day}")["Close"]
+
+# Calcular retornos diários
+returns = data.pct_change().dropna()
+
+# Calcular a correlação
+correlation = returns.corr().iloc[0, 1]
+
+print(f"Correlação entre {ticker1} e {ticker2}: {correlation:.4f}")'''
+
--- forecasting_jupiter/correlation/correlacao_sectores.py
+++ forecasting_jupiter/correlation/correlacao_sectores.py
@@ -7,6 +7,18 @@ from datetime import datetime
 from yahooquery import Screener
 
 # %%
+sector_mapping2 = [
+        'basic_materials',
+        'communication_services',
+        'consumer_cyclical',
+        'consumer_defensive',
+        'energy',
+        'financial_services',
+        'healthcare',
+        'industrials',
+        'real_estate',
+        'technology',
+        'utilities']
 def get_companiesbysector(sector):  # Obtém 100 maiores empresas do determinado setor
     sector_mapping = {
         'basic_materials': 'ms_basic_materials',
@@ -37,62 +49,64 @@ def get_companiesbysector(sector):  # Obtém 100 maiores empresas do determinado
     sector_tickers = [stock['symbol'] for stock in data[screener_id]['quotes']]
     return sector_tickers
 
-# %%
-# Obtendo tickers dos setores desejados
-ticker1 = get_companiesbysector('energy')
-ticker2 = get_companiesbysector('communication_services')
-ticker1.remove('WDS')
-# %%
-# Baixar os dados históricos para as empresas dos setores indicados
-day, month, year = datetime.now().day, datetime.now().month, datetime.now().year
-start_date = f"{year-40}-{month}-{day}"  # 40 anos atrás
-end_date = f"{year}-{month}-{day}"
+# %% 
+# Função mãe
+def mae(dataset1, dataset2):
 
+    # Preencher dados ausentes com o último valor disponível
+    dataset1 = dataset1.fillna(1)
+    dataset2 = dataset2.fillna(1)
+
+
+    # Calcular as percentagens diárias
+    returns1 = dataset1.pct_change().dropna()
+    returns2 = dataset2.pct_change().dropna()
+
+
+    correlation_by_company = {}
+    for company1 in returns1.columns:
+        correlation_by_company[f'{company1}'] = {}
+        for company2 in returns2.columns:
+            merged_df = pd.concat([returns1[company1], returns2[company2]], axis=1)
+            correlation_by_company[f'{company1}'][f'{company2}'] = merged_df[company1].corr(merged_df[company2])
+
+
+    # Converter o dicionário de correlações em um DataFrame
+    correlation_df = pd.DataFrame.from_dict(correlation_by_company, orient='index')
+
+
+    # Gerar o heatmap
+    plt.figure(figsize=(40, 40))
+    sns.heatmap(correlation_df, annot=False, cmap='coolwarm', center=0, linewidths=0.5, cbar=True)
+
+    # Personalizar o gráfico
+    plt.title(f'Correlação Anual: Empresas do Setor {sector_mapping2[i]} vs. {sector_mapping2[j]}')
+    plt.xlabel('Ano')
+    plt.ylabel('Empresas')
+
+    # Exibir o gráfico
+    plt.show()
+
+# %%
 # Baixar os dados usando Yahoo Finance
 def download_data(tickers, start, end):
     return yf.download(tickers, start=start, end=end)['Close']
 
-dataset1 = download_data(ticker1, start_date, end_date)
-dataset2 = download_data(ticker2, start_date, end_date)
-
-# Concatenar os datasets em um único DataFrame
-#dataset = pd.concat([dataset1, dataset2], axis=1)
-
+# Baixar os dados históricos para as empresas dos setores indicados
+day, month, year = datetime.now().day, datetime.now().month, datetime.now().year
+start_date = f"{year-40}-{month}-{day}"  # 40 anos atrás
+end_date = f"{year}-{month}-{day}"
+# %%
+datasetpro = []
+for i in range(len(sector_mapping2)):
+    ticker_pro = get_companiesbysector(sector_mapping2[i])
+    datasetpro.append(download_data(ticker_pro, start_date, end_date))
 
 # %%
-# Preencher dados ausentes com o último valor disponível
-dataset1 = dataset1.fillna(1)
-dataset2 = dataset2.fillna(1)
-
-
-# %%
-# Calcular as percentagens diárias
-returns1 = dataset1.pct_change().dropna()
-returns2 = dataset2.pct_change().dropna()
-
-# %%
-correlation_by_company = {}
-for company1 in returns1.columns:
-    correlation_by_company[f'{company1}'] = {}
-    for company2 in returns2.columns:
-        merged_df = pd.concat([returns1[company1], returns2[company2]], axis=1)
-        correlation_by_company[f'{company1}'][f'{company2}'] = merged_df[company1].corr(merged_df[company2])
-
-# %%
-# Converter o dicionário de correlações em um DataFrame
-correlation_df = pd.DataFrame.from_dict(correlation_by_company, orient='index')
-
-# %%
-# Gerar o heatmap
-plt.figure(figsize=(40, 40))
-sns.heatmap(correlation_df, annot=False, cmap='coolwarm', center=0, linewidths=0.5, cbar=True)
-
-# Personalizar o gráfico
-plt.title('Correlação Anual: Empresas do Setor Financeiro vs. Energia')
-plt.xlabel('Ano')
-plt.ylabel('Empresas')
-
-# Exibir o gráfico
-plt.show()
-
-# %%
+for i in range(len(sector_mapping2)):
+    # Obtendo tickers dos setores desejados
+    dataset1 = datasetpro[i]
+    for j in range(len(sector_mapping2)):
+        if i != j:
+            dataset2 = datasetpro[j]
+            mae(dataset1, dataset2)
