b51026f4929d4bed22d5c3d70ef08a577660854b
Pedro Maltez - Ubuntu <pedromaltez16.1@gmail.com>
2025-04-13 03:08:46 +0100

    linear regression to log sp50

--- flask_app/help/linear_regression.py
+++ flask_app/help/linear_regression.py
@@ -2,6 +2,8 @@
 # https://medium.com/data-hackers/implementando-regress%C3%A3o-linear-simples-em-python-91df53b920a8
 # https://www.datacamp.com/pt/tutorial/linear-regression-in-python
 # https://brains.dev/2022/pratica-regressao-linear-com-codigo-em-python/
+
+
 # %%
 import matplotlib.pyplot as plt
 import yfinance as yf
@@ -14,7 +16,7 @@ import numpy as np
 
 # %%
 # Função para obter os dados históricos do S&P 500
-def get_data(symbol='^GSPC', period='120y', interval='1d'):
+def get_data(symbol='^GSPC', period='80y', interval='1d'):
     data = yf.download(tickers=symbol, period=period, interval=interval)
     data = data[['Close']].reset_index()
     return data
@@ -23,72 +25,80 @@ def get_data(symbol='^GSPC', period='120y', interval='1d'):
 # sp500_data
 # Obter dados históricos do S&P 500
 sp500_data = get_data()
-sp500_data
+#sp500_data
 
 
 # %%
 # unit_of_time
 # Criar um vetor de anos com base no número de dados
 unit_of_time = np.arange(len(sp500_data))
-unit_of_time
+#unit_of_time
 
 
 # %%
 # log_sp500
 # Criar log do sp500
 log_sp500 = np.log(sp500_data['Close'])
-log_sp500
+#log_sp500
 
 
 # %%
 # Regressão linear simples
 # Sample data
 x = unit_of_time
-y = log_sp500
+y_log = log_sp500
+y = np.array(sp500_data['Close'])
 
 # %%
 # x,y
 # Garantir que ambos são arrays numpy para evitar erros de broadcasting
 x = np.array(x)
-y = np.array(y)
+y_log = np.array(y_log)
 
-x,y
+
+#x, y_log, y
 # %%
 # x_mean,y_mean
 # Compute means
 x_mean = np.mean(x)
 y_mean = np.mean(y)
+y_mean_log = np.mean(y_log)
 
-x_mean,y_mean
+#x_mean,y_mean_log
 
 
 # %%
 # Example of NumPy's polyfit
-coef = np.polyfit(x, y, 1)
-y_pred2 = np.polyval(coef, x)
+coef_log = np.polyfit(x, y_log, 1)
+y_pred_log = np.polyval(coef_log, x)
+
+y_pred = np.exp(y_pred_log)
+#y_pred
 
 
 # %%
+'''
 ## PORQUE ESTÁ ERRADO?????
 # Compute slope (B1)
-B1 = np.sum((x - x_mean) * (y - y_mean)) / np.sum((x - x_mean) ** 2)
+B1 = np.sum((x - x_mean) * (y_log - y_mean_log)) / np.sum((x - x_mean) ** 2)
 
 # Compute intercept (B0)
-B0 = y_mean - B1 * x_mean
+B0 = y_mean_log - B1 * x_mean
 
 print(f"Slope (B1): {B1}")
 print(f"Intercept (B0): {B0}")
-
+'''
 # %%
+'''
 # Predicting values
 y_pred = B0 + B1 * x
 print("Predicted values:", y_pred)
+'''
 
 # %%
 # Plotando os gráficos
 plt.figure(figsize=(12, 6))
-plt.plot(sp500_data['Date'], y_pred2, label='Crescimento Exponencial', linestyle='dashed', color='red')
-plt.plot(sp500_data['Date'], y_pred, label='Crescimento Exponencial', linestyle='dashed', color='red')
+plt.plot(sp500_data['Date'], y_pred_log, label='Crescimento Exponencial', linestyle='dashed', color='red')
 plt.plot(sp500_data['Date'], log_sp500, label='Crescimento Exponencial', linestyle='dashed', color='red')
 # Melhorando visualmente
 plt.xticks(rotation=45)
@@ -99,3 +109,21 @@ plt.legend()
 plt.grid()
 plt.show()
 
+# %%
+# Plotando os gráficos
+plt.figure(figsize=(12, 6))
+plt.plot(sp500_data['Date'], y_pred, label='Crescimento Exponencial', linestyle='dashed', color='red')
+plt.plot(sp500_data['Date'], y, label='Crescimento Exponencial', linestyle='dashed', color='red')
+# Melhorando visualmente
+plt.xticks(rotation=45)
+plt.xlabel('Ano')
+plt.ylabel('Valor')
+plt.title('Comparação entre o S&P 500 e uma curva exponencial')
+plt.legend()
+plt.grid()
+plt.show()
+
+
+# %%
+percent = 1
+print(f"Rendimento médio do sp500: {percent}%")
