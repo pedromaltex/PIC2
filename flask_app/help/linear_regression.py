ad1d13ca4fc28d7076d33db9af3a56375082db62
Pedro Maltez - Ubuntu <pedromaltez16.1@gmail.com>
2025-04-18 17:49:31 +0100

    Novas funções importantes, allocation e outras

--- flask_app/help/linear_regression.py
+++ flask_app/help/linear_regression.py
@@ -24,11 +24,11 @@ def get_data(symbol='^GSPC', period='80y', interval='1mo'):
 # %%
 # sp500_data
 # Obter dados históricos do S&P 500
-name, periodo, intervalo = '^GSPC', '150y', '1mo'
+name, periodo, intervalo = '^GSPC', '5y', '1mo'
 sp500_data = get_data(name, periodo, intervalo)
 name = yf.Ticker(name).info['longName']
 
-sp500_data
+#sp500_data
 
 
 # %%
@@ -52,7 +52,7 @@ x = unit_of_time
 y_log = log_sp500
 y = np.array(sp500_data['Close'])
 y = y.flatten()
-y
+#y
 
 # %%
 # x,y
@@ -77,13 +77,13 @@ coef_log = np.polyfit(x, y_log, 1)
 y_pred_log = np.polyval(coef_log, x)
 coef_log = coef_log.flatten()
 y_pred = np.exp(y_pred_log)
-y_pred
+#y_pred
 
 # %%
 # SP500 - exponential 
 
 diference = 100* (y - y_pred)/y_pred
-diference
+#diference
 # %%
 '''
 ## PORQUE ESTÁ ERRADO?????
@@ -155,8 +155,6 @@ plt.title(f'Exponential vs {name} (Diference)')
 
 improve_draw()
 
-
-
 # %%
 # Calcular rendimento médio do sp500, apenas funciona com períodos em meses
 y1 = y_pred[-11]
@@ -174,5 +172,68 @@ cagr = (1 + growth_rate)**12 - 1  # anualizado
 
 print(f"Crescimento médio mensal: {growth_rate * 100:.2f}%")
 print(f"Crescimento médio anual (CAGR): {cagr * 100:.2f}%")
+
+# %% # MÉTODO TESTE
+monthly_investment = 30000/len(y)
+total_invest = np.zeros(len(y))
+total_invest[0] = monthly_investment
+for i in range(1, len(total_invest)):
+    total_invest[i] = total_invest[i-1] + monthly_investment
+total_invest
+
+# %%
+sp500_price = y / 10
+
+stocks_owned = monthly_investment / sp500_price # Quantas ações consigo comprar com 500 euros
+for i in range(len(stocks_owned)-1):
+    stocks_owned[i+1] += stocks_owned[i] # Tornar a função acumulativa
+
+
+# %%
+porfolio = stocks_owned * sp500_price # Calcular evolução portfolio
+
+# %%
+
+# %%
+# Método de weighted buy
+allocation = (monthly_investment * (1 - 2.5 * diference/100)) # dinheiro investido mês a mês
+total_allocation = np.zeros(len(allocation))
+for i in range(len(allocation)):
+    if allocation[i] < 0:
+        allocation[i] = 0 # Não retirar dinheiro para não pagar impostos
+    total_allocation[i] = sum(allocation[:i+1])
+
+total_allocation 
+
+#allocation
+# %%
+stocks_owned2 = allocation / sp500_price
+#stocks_owned2
+# %%
+for i in range(len(stocks_owned2)-1):
+    stocks_owned2[i+1] += stocks_owned2[i]
+#stocks_owned2
+# %%
+porfolio2 = stocks_owned2 * sp500_price
+porfolio2
+
+# %% 
+print(f"Totalidade de dinheiro alocado em Standart Investment: {total_invest[-1]}.")
+print(f"Totalidade de carteira de investimento em Standart Investment: {porfolio[-1]}.")
+
+print(f"Totalidade de dinheiro alocado: {total_allocation[-1]}.")
+print(f"Totalidade de carteira de investimento: {porfolio2[-1]}.")
+# %%
+plt.figure(figsize=(12, 6))
+plt.plot(sp500_data['Date'], porfolio, label='Standart Investment', linestyle='solid', color='red')
+plt.plot(sp500_data['Date'], porfolio2, label="Maltez's way", linestyle='dotted', color='blue')
+plt.title("Standart Investment vs Maltez's way")
+improve_draw()
+# %%
+plt.figure(figsize=(12, 6))
+plt.plot(sp500_data['Date'], total_invest, label='Standart Investment (Allocation)', linestyle='solid', color='red')
+plt.plot(sp500_data['Date'], total_allocation, label="Maltez's way (Allocation)", linestyle='dotted', color='blue')
+plt.title("Allocation")
+improve_draw()
 
 # %%
